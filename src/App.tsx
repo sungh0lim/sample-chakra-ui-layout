d2a9c5943af4d53c5ddb96152967900b5c2faa00
임성호 <sungho.lim1@wesang.com>
2024-04-11 00:27:38 +0900

    add layout test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Box, Center, Flex, Square } from "@chakra-ui/layout";
+
 function App() {
-  return <>Hello, World</>;
+  return (
+    <div style={{ backgroundColor: "#000" }}>
+      <Flex color="white">
+        <Center w="100px" bg="green.500">
+          Box 1
+        </Center>
+        <Square bg="blue.500" size="150px">
+          Box 2
+        </Square>
+        <Box flex="1" bg="tomato">
+          Box 3
+        </Box>
+      </Flex>
+    </div>
+  );
 }
 
 export default App;
